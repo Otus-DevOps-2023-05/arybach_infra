e806114d136c944e7217bb05246a473fbe14d8f3
Alex R <rybachek155@gmail.com>
2023-06-29 12:12:38 +0800

    trial and error #7

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
+# Import the MongoDB public GPG key
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
+
+# Add the MongoDB repository
+echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
+
+# Update the package repository
 sudo apt-get update
-sudo apt-get install --reinstall apt-transport-https
 
-wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
-echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
-
+# Install MongoDB
 sudo apt-get install -y mongodb-org
 
+# Start MongoDB service
 sudo systemctl start mongod
+
+# Enable MongoDB service to start on system boot
 sudo systemctl enable mongod
+
+# Check MongoDB service status
 sudo systemctl status mongod
