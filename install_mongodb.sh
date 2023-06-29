04b3639381d6d643749ab37bee7e80d3cccae4ce
Alex R <rybachek155@gmail.com>
2023-06-29 13:45:46 +0800

    trial and error #10

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
 wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
 
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
