5de1868467df3b0684d0a1ba81479a2ba1a127bc
Alex R <rybachek155@gmail.com>
2023-06-29 17:58:50 +0800

    trial and error #12

--- deploy.sh
+++ deploy.sh
@@ -6,8 +6,11 @@ sudo apt install -y git
 ### Clone app code
 git clone -b monolith https://github.com/express42/reddit.git
 
+### Change to the app directory
+cd reddit
+
 ### Install app dependencies
-cd reddit && bundle install
+bundle install
 
 ### Launch app
 puma -d
