6ef078271a41545e57be8126cf2dfd7c2db40c07
Alex R <rybachek155@gmail.com>
2023-07-24 14:02:59 +0800

    sorted out ynadex provider local override + versions

--- terraform/prod/main.tf
+++ terraform/prod/main.tf
@@ -1,11 +1,10 @@
 terraform {
-  required_version = "0.13.1"
+  required_version = ">= 0.13.1"
 
   required_providers {
     yandex = {
       source  = "yandex-cloud/yandex"
-      version = "0.93.0"
-      # version = "> 0.35"
+      version = ">= 0.35"
     }
   }
 }
